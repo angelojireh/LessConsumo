ad43ecce4d4b20bed5aa5e9b83d20a4329aeabc9
angelo <angelojirehfidel@gmail.com>
2020-12-16 09:51:20 +0800

    navigation functionality for product, shipping and information activities

--- app/src/main/java/com/ecommerce/lessconsumo/activity/ProductActivity.kt
+++ app/src/main/java/com/ecommerce/lessconsumo/activity/ProductActivity.kt
@@ -3,32 +3,39 @@ package com.ecommerce.lessconsumo.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.ecommerce.lessconsumo.R
 import kotlinx.android.synthetic.main.activity_product.*
 
-class ProductActivity : AppCompatActivity() {
+class ProductActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product)
 
-        onClicks()
+        initOnClickListeners()
 
     }
 
-    private fun onClicks() {
-        // setup
-        buttonBack.setOnClickListener {
-            finishMe()
+    override fun onClick(v: View?) {
+        when(v?.id)
+        {
+            R.id.buttonBackProduct -> finishMe()
+            R.id.buttonAddToBag -> addToBag()
         }
     }
 
-    /*private fun gotoNewActivity(activity : Activity) {
-        val intent = Intent (this, activity::class.java)
-        startActivity(intent)
-    }*/
+    private fun initOnClickListeners() {
+        buttonBackProduct.setOnClickListener(this)
+        buttonAddToBag.setOnClickListener(this)
+    }
 
     private fun finishMe()
     {
         this.finish()
+    }
+
+    private fun addToBag()
+    {
+        // add to bag functionality
     }
 }
--- app/src/main/java/com/ecommerce/lessconsumo/activity/ShippingActivity.kt
+++ app/src/main/java/com/ecommerce/lessconsumo/activity/ShippingActivity.kt
@@ -1,12 +1,42 @@
 package com.ecommerce.lessconsumo.activity
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.ecommerce.lessconsumo.R
+import kotlinx.android.synthetic.main.activity_shipping.*
 
-class ShippingActivity : AppCompatActivity() {
+class ShippingActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shipping)
+
+        initOnClickListeners()
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id)
+        {
+            R.id.buttonBackShipping -> finishMe()
+            R.id.buttonContinue -> gotoNewActivity(PaymentActivity())
+        }
+    }
+
+    private fun initOnClickListeners()
+    {
+        buttonBackShipping.setOnClickListener(this)
+        buttonContinue.setOnClickListener(this)
+    }
+
+    private fun gotoNewActivity(activity : Activity) {
+        val intent = Intent (this, activity::class.java)
+        startActivity(intent)
+    }
+
+    private fun finishMe()
+    {
+        this.finish()
     }
 }
