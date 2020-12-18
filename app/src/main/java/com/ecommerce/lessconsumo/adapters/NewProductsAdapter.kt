811040677422d5c3ca9288f636726fbe545a5b3a
angelo <angelojirehfidel@gmail.com>
2020-12-18 19:05:43 +0800

    saleprice functionality and regularprice strikethrough

--- app/src/main/java/com/ecommerce/lessconsumo/adapters/NewProductsAdapter.kt
+++ app/src/main/java/com/ecommerce/lessconsumo/adapters/NewProductsAdapter.kt
@@ -3,6 +3,7 @@ package com.ecommerce.lessconsumo.adapters
 import android.app.Activity
 import android.content.Intent
 import android.text.Html
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
